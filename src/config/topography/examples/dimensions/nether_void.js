d9f257a1c3c6ca2e6e757ec9f0c0e630e75dc559
Blargerist Platypus <yaywarcraft@gmail.com>
2021-03-27 14:07:21 -0600

    Reduce nether biome size from 6 to 4 Reduce structure spacing for fortress and bastion

--- src/config/topography/examples/dimensions/nether_void.js
+++ src/config/topography/examples/dimensions/nether_void.js
@@ -1,7 +1,13 @@
+
+//structure, average distance between placement attempts in chunks, minimum distance between two structures of type in chunks, some seed modifier number
+//second number must be less than first number
+setStructureSpacing("minecraft:fortress", 10, 4, 30084232);
+setStructureSpacing("minecraft:bastion_remnant", 5, 3, 30084232);
+
 function buildChunkGenerator(seed, biomeRegistry, dimensionSettingsRegistry) {
 	var biomes = BiomeHelper.forNether();
 	
-	var biomeProvider = new MultiBiomeProvider(biomes, seed, 6, biomeRegistry);
+	var biomeProvider = new MultiBiomeProvider(biomes, seed, 4, biomeRegistry);
 	
 	return new ChunkGeneratorVoid(biomeProvider, function() {
 		return RegistryHelper.get(dimensionSettingsRegistry, "minecraft:nether");
