fbeef8131e3b15fc81ef6946a5db0dc2fd075313
Blargerist Platypus <yaywarcraft@gmail.com>
2021-03-27 14:05:56 -0600

    Reduce end biome size from 6 to 4

--- src/config/topography/examples/dimensions/end_void.js
+++ src/config/topography/examples/dimensions/end_void.js
@@ -1,7 +1,7 @@
 function buildChunkGenerator(seed, biomeRegistry, dimensionSettingsRegistry) {
 	var biomes = BiomeHelper.forEnd();
 	
-	var biomeProvider = new MultiBiomeProvider(biomes, seed, 6, biomeRegistry);
+	var biomeProvider = new MultiBiomeProvider(biomes, seed, 4, biomeRegistry);
 	
 	return new ChunkGeneratorVoid(biomeProvider, function() {
 		return RegistryHelper.get(dimensionSettingsRegistry, "minecraft:end");
