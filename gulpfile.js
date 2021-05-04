b9ff671f9720bda3a22a517688b9d6a4f77be0a8
Justin Russo <j.russo@outlook.com>
2021-05-03 23:15:28 -0400

    build: add build scripts to minify & add license to supported files

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const del = require("del");
+const { src, dest, parallel, series } = require("gulp");
+const gulpif = require("gulp-if");
+const uglify = require("gulp-uglify");
+const jsonMinify = require("gulp-json-minify");
+const license = require("./tools/gulp-utils/license");
+
+const outputBase = "dist";
+
+const srcPaths = {
+  config: "src/config/**/*",
+  defaultConfigs: "src/defaultconfigs/**/*",
+  js: "src/**/*.js",
+  json: "src/**/*.json",
+};
+
+const fileChecker = {
+  cfg: (file) => file.extname === ".cfg",
+  toml: (file) => file.extname === ".toml",
+};
+
+const buildConfig = () => {
+  return src([srcPaths.config, "!**/*.js", "!**/*.json"])
+    .pipe(gulpif(fileChecker.cfg, license("hash")))
+    .pipe(gulpif(fileChecker.toml, license("hash")))
+    .pipe(dest(path.join(outputBase, "config")));
+};
+
+const buildDefaultConfigs = () => {
+  return src([srcPaths.defaultConfigs, "!**/*.js", "!**/*.json"])
+    .pipe(gulpif(fileChecker.cfg, license("hash")))
+    .pipe(gulpif(fileChecker.toml, license("hash")))
+    .pipe(dest(path.join(outputBase, "defaultconfigs")));
+};
+
+const buildJavaScript = () => {
+  return src(srcPaths.js)
+    .pipe(uglify())
+    .pipe(license("js"))
+    .pipe(dest(outputBase));
+};
+
+const buildJson = () => {
+  return src(srcPaths.json)
+    .pipe(license("json"))
+    .pipe(jsonMinify())
+    .pipe(dest(outputBase));
+};
+
+const clean = () => {
+  return del(outputBase);
+};
+
+const copyRemaining = () => {
+  const excludes = Object.values(srcPaths).map((path) => `!${path}`);
+  return src(["src/**/*", ...excludes]).pipe(dest(outputBase));
+};
+
+exports.default = exports.build = series(
+  clean,
+  parallel(
+    buildJavaScript,
+    buildJson,
+    buildConfig,
+    buildDefaultConfigs,
+    copyRemaining
+  )
+);
---
+++ tools/gulp-utils/license.js
@@ -0,0 +1,82 @@
+const json5 = require("json5");
+const through = require("through2");
+
+function prefixStream(prefixText) {
+  var stream = through();
+  stream.write(prefixText);
+  return stream;
+}
+
+const hashLicense = () =>
+  `##########
+# Copyright (c) ${new Date().getFullYear()} Darkosto Entertainment LLC
+#
+# All rights reserved.
+##########
+
+`;
+
+const jsLicense = () =>
+  `/*!
+ * Copyright (c) ${new Date().getFullYear()} Darkosto Entertainment LLC
+ *
+ * All rights reserved.
+ */
+
+`;
+
+const jsonLicense = () => ({
+  line1: `Copyright (c) ${new Date().getFullYear()} Darkosto Entertainment LLC`,
+  line2: "All rights reserved.",
+});
+
+/**
+ *
+ * @param {*} licenseStyle "hash" | "js"
+ * @returns
+ */
+module.exports = (licenseStyle) =>
+  through.obj((file, encoding, callback) => {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    let template;
+    switch (licenseStyle) {
+      case "hash":
+        template = hashLicense;
+        break;
+      case "js":
+        template = jsLicense;
+        break;
+      case "json":
+        template = jsonLicense;
+        break;
+      default:
+        return callback(new Error("Unsupported license style"));
+    }
+
+    if (file.isBuffer()) {
+      if (licenseStyle === "json") {
+        const json = json5.parse(file.contents.toString());
+
+        file.contents = Buffer.from(
+          JSON.stringify({
+            __copyright: template(),
+            ...json,
+          })
+        );
+      } else {
+        file.contents = Buffer.from(template() + file.contents);
+      }
+    }
+
+    if (file.isStream()) {
+      if (licenseStyle === "json") {
+        return callback(new Error("JSON does not support streams"));
+      }
+      file.contents = file.contents.pipe(prefixStream(template()));
+    }
+
+    return callback(null, file);
+  });
