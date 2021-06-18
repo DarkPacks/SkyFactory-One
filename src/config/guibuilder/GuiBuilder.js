6b4530ac1667b04aab7dc4a77390a9a17e9c6906
Blargerist Platypus <yaywarcraft@gmail.com>
2021-06-17 19:15:44 -0600

    Add texture to Supporters button. Still needs hover texture

--- src/config/guibuilder/GuiBuilder.js
+++ src/config/guibuilder/GuiBuilder.js
@@ -1,6 +1,7 @@
 
 //Gui to add button to, script file, anchor location, width, height, text
-addButton("ExtendedMenuScreen", "SupporterUI", EnumGuiLocation.RIGHT, 98, 20, "Supporters").setAbsXOffset(-82).setAbsYOffset(120);
+//addButton("ExtendedMenuScreen", "SupporterUI", EnumGuiLocation.RIGHT, 98, 20, "Supporters").setAbsXOffset(-82).setAbsYOffset(120);
+addButton("ExtendedMenuScreen", "SupporterUI", EnumGuiLocation.RIGHT, 98, 20).title("Supporters").texture("packmenu:textures/gui/button_small.png", "minecraft:textures/block/jack_o_lantern.png").setAbsXOffset(-82).setAbsYOffset(120);
 
 //Gui to add button to, script file, anchor location, width, height, image, hover image
 //addImgButton("ExtendedMenuScreen", "SupporterUI", EnumGuiLocation.RIGHT, 20, 20, "minecraft:textures/block/green_wool.png", "minecraft:textures/block/jack_o_lantern.png").setAbsXOffset(-185).setAbsYOffset(-32);
